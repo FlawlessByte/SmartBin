c96f6d31fd6b9a13ed655b56800e01703ddcc72e
realinventor <jimmyose009@gmail.com>
2019-03-10 11:55:11 +0530

    initial commit

---
+++ app/src/main/java/co/realinventor/smartbin/MainActivity.java
@@ -0,0 +1,23 @@
+package co.realinventor.smartbin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void userModeSelected(View view){
+
+    }
+
+    public void adminModeSelected(View view){
+        
+    }
+}
